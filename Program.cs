10d015f79d64f63f50eee0c5ff87cedca5b9bebe
realswoosh <realswoosh@gmail.com>
2018-02-13 11:53:58 +0900

    Source Line Count 추가

--- Program.cs
+++ Program.cs
@@ -15,13 +15,15 @@ namespace dmLineCounter
 		static string SearchPattern { get; set; } = "*";
 		static int Count = 0;
 
+		static int sourceLine = 0;
+
 		static void Main(string[] args)
 		{
 			dmCommandLineParser.Parser lineParser = new dmCommandLineParser.Parser();
 
 			lineParser.Add(new dmCommandLineParser.Option("-path", new dmCommandLineParser.Operator.FuncOperator(SetPath)));
 			lineParser.Add(new dmCommandLineParser.Option("-pattern", new dmCommandLineParser.Operator.FuncOperator((string arg) => { if (arg != "") SearchPattern = arg; })));
-
+			
 			if (args.Length > 0)
 			{
 				lineParser.Process(args);
@@ -29,13 +31,15 @@ namespace dmLineCounter
 
 			if (string.IsNullOrEmpty(Path))
 				Path = Environment.CurrentDirectory;
-
+						
 			Stopwatch stopWatch = new Stopwatch();
 			stopWatch.Start();
 
 			var directoryList = Searcher.GetDirectories(Path, searchOption : SearchOption.AllDirectories);
 
 			SemaphoreSlim maxThread = new SemaphoreSlim(10);
+
+			var sourceExts = new[] { ".c", ".cpp", ".hpp", ".cs" };
 
 			foreach (var tmpPath in directoryList)
 			{
@@ -44,9 +48,20 @@ namespace dmLineCounter
 				Task.Factory.StartNew(() =>
 				{
 					Console.Write(".");
-
 					var files = Directory.GetFiles(tmpPath, SearchPattern);
 					Interlocked.Add(ref Count, files.Count());
+
+					Parallel.ForEach(files, x =>
+					{
+						string ext = System.IO.Path.GetExtension(x);
+
+						if (Array.Exists(sourceExts, a => { return a == ext; }))
+						{
+							int lineLength = File.ReadAllLines(x).Length;
+							Interlocked.Add(ref sourceLine, lineLength);
+						}
+					});
+					
 				}, TaskCreationOptions.LongRunning)
 				.ContinueWith((task) => maxThread.Release());
 			}
@@ -56,9 +71,8 @@ namespace dmLineCounter
 			Console.WriteLine("");
 			Console.WriteLine($"Root : {Path}");
 			Console.WriteLine($"SearchPattern : {SearchPattern}");
-			Console.WriteLine($"Search Duration : {stopWatch.ElapsedMilliseconds}, FileCount={Count}");
+			Console.WriteLine($"Search Duration : {stopWatch.ElapsedMilliseconds}, FileCount={Count}, LineCount={sourceLine}");
 		}
-
 
 		static void SetPath(string arg)
 		{
